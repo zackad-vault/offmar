46108839c927eadb76d26d5fa632b7cd76c7eee7
zackad <cahsiwarak@gmail.com>
2015-05-17 21:16:26 +0700

    [draft] natural sort

--- ofmar.user.js
+++ ofmar.user.js
@@ -2,7 +2,7 @@
 // @name           Manga Reader Offline
 // @namespace      zackad's script
 // @author         zackad
-// @version        0.2.4
+// @version        0.2.3
 // @description    read manga offline from your folder collection
 // @include        file:///*/*
 // @copyright      2015 zackad
@@ -16,7 +16,7 @@ $(document).ready(function(){
 		+'<style type="text/css">'
 		+'.location-container a {color:blue; font-weight:bold; text-decoration:none}'
 		+'.location-container a.current {color:red;}'
-        +'img {max-width:90%!important; max-height:900px!important;}'
+        +'img {max-width:90%!important; max-height:1000px!important;}'
         +'body {max-width:100%!important;}'
 		+'</style>'
 		;
@@ -105,6 +105,9 @@ $(document).ready(function(){
         $('body').prepend(container);
 		$('body').prepend(loc_wrp);
         $('.container-container').append(controler);
+		/* var fLink = $('table a');
+		fLink.href.sort(naturalSort);
+		clog(fLink.href); */
 	}
     function lets_roll(){
 		if (img.length == 0) return;
@@ -113,6 +116,55 @@ $(document).ready(function(){
         getLoc();
         $('#enable').hide();
 	}
+/*
+ * Natural Sort algorithm for Javascript - Version 0.8 - Released under MIT license
+ * Author: Jim Palmer (based on chunking idea from Dave Koelle)
+ */
+function naturalSort (a, b) {
+    var re = /(^([+\-]?(?:\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?)?$|^0x[\da-fA-F]+$|\d+)/g,
+        sre = /^\s+|\s+$/g,   // trim pre-post whitespace
+        snre = /\s+/g,        // normalize all whitespace to single ' ' character
+        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
+        hre = /^0x[0-9a-f]+$/i,
+        ore = /^0/,
+        i = function(s) {
+            return (naturalSort.insensitive && ('' + s).toLowerCase() || '' + s).replace(sre, '');
+        },
+        // convert all to strings strip whitespace
+        x = i(a) || '',
+        y = i(b) || '',
+        // chunk/tokenize
+        xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        // numeric, hex or date detection
+        xD = parseInt(x.match(hre), 16) || (xN.length !== 1 && Date.parse(x)),
+        yD = parseInt(y.match(hre), 16) || xD && y.match(dre) && Date.parse(y) || null,
+        normChunk = function(s, l) {
+            // normalize spaces; find floats not starting with '0', string or 0 if not defined (Clint Priest)
+            return (!s.match(ore) || l == 1) && parseFloat(s) || s.replace(snre, ' ').replace(sre, '') || 0;
+        },
+        oFxNcL, oFyNcL;
+    // first try and sort Hex codes or Dates
+    if (yD) {
+        if ( xD < yD ) { return -1; }
+        else if ( xD > yD ) { return 1; }
+    }
+    // natural sorting through split numeric strings and default strings
+    for(var cLoc=0, xNl = xN.length, yNl = yN.length, numS=Math.max(xNl, yNl); cLoc < numS; cLoc++) {
+        oFxNcL = normChunk(xN[cLoc], xNl);
+        oFyNcL = normChunk(yN[cLoc], yNl);
+        // handle numeric vs string comparison - number < string - (Kyle Adams)
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) { return (isNaN(oFxNcL)) ? 1 : -1; }
+        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
+        else if (typeof oFxNcL !== typeof oFyNcL) {
+            oFxNcL += '';
+            oFyNcL += '';
+        }
+        if (oFxNcL < oFyNcL) { return -1; }
+        if (oFxNcL > oFyNcL) { return 1; }
+    }
+    return 0;
+}
     init();
     getLoc();
     $('#disable').click(disable);
@@ -129,7 +181,7 @@ $(document).ready(function(){
     if(keyCode == 13 ){
         lets_roll();
     }
-	// caseof : \ 'backslash'
+        // caseof : \ 'backslash'
     if(keyCode == 220){
         if(img.length == 0) return;
         window.location.reload();
