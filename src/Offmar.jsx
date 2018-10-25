70c672a9cb95bf8c243acac108d95ea8fb1162e4
zackad <zackad.dev@gmail.com>
2018-10-25 13:00:05 +0700

    initial code

---
+++ src/Offmar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Offmar extends React.Component {
+  render () {
+    return (
+      <div>Offline Manga Reader</div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import * as Prepare from './utils/prepare'
+import Offmar from './Offmar.jsx'
 
 Prepare.cleanup()
 Prepare.insertRootContainer()
+
+ReactDOM.render(<Offmar />, document.getElementById('root'))
