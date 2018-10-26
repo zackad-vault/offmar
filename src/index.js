d8c33086be654c3841144f74047842305d29f9d5
zackad <zackad.dev@gmail.com>
2018-10-26 16:26:07 +0700

    pass list item as props to app component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import * as Prepare from './utils/prepare'
 import Offmar from './Offmar.jsx'
 
+const listItems = Prepare.getListItems()
+
 Prepare.cleanup()
 Prepare.insertRootContainer()
 
-ReactDOM.render(<Offmar />, document.getElementById('root'))
+ReactDOM.render(<Offmar listItems={listItems} />, document.getElementById('root'))
