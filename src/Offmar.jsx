447c6e73ad70fa43af06cedcd34a31953813a440
zackad <zackad.dev@gmail.com>
2018-10-26 16:27:31 +0700

    pass list item into DirListing component

--- src/Offmar.jsx
+++ src/Offmar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import DirListing from './components/DirListing.jsx'
 
 export default class Offmar extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      mode: 'list'
+    }
+  }
+
   render () {
     return (
-      <div>Offline Manga Reader</div>
+      (this.state.mode === 'list') ? <DirListing items={this.props.listItems}/> : false
     )
   }
 }
