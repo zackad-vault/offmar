cb49b7266685abe5eab3f05f71f93455991a67c1
zackad <zackad.dev@gmail.com>
2018-10-26 14:43:11 +0700

    additional metadata

--- src/utils/metadata.js
+++ src/utils/metadata.js
@@ -2,10 +2,14 @@ const Meta = require('userscript-metadata')
 const pkg = JSON.parse(JSON.stringify(require('../../package.json')))
 
 let Metadata = {
-  name: pkg.name,
-  version: pkg.version,
+  name: pkg.longName,
+  namespace: pkg.namespace,
   author: pkg.author,
+  version: pkg.version,
   description: pkg.description,
+  include: pkg.include,
+  exclude: pkg.exclude,
+  grant: pkg.grant,
   run_at: 'document-start',
   license: pkg.license
 }
