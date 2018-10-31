5f803b538a22d634818f2f3bcf2f39f47933a3d4
zackad <zackad.dev@gmail.com>
2018-10-31 15:04:58 +0700

    sorting list entry with natural order

--- src/components/DirListing.jsx
+++ src/components/DirListing.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import natsort from 'natsort'
 import ListItem from './ListItem.jsx'
 
 export default class DirListing extends React.Component {
   render () {
-    const directory = this.props.items.filter(item => item.className === 'dir')
-    const file = this.props.items.filter(item => item.className === 'file')
+    const directory = this.props.items
+      .filter(item => item.className === 'dir')
+      .sort(natsort({ insensitive: true }))
+    const file = this.props.items
+      .filter(item => item.className === 'file')
+      .sort(natsort({insensitive: true}))
     return (
       <div>
         {directory.map(item => <ListItem item={item} key={item.href}/>)}
