389922c21148ae9df5b45ad7c07c11764ba6178c
zackad <zackad.dev@gmail.com>
2018-11-23 14:15:24 +0700

    [refactor] remove natural sorting from DirListing component

--- src/components/DirListing.jsx
+++ src/components/DirListing.jsx
@@ -1,28 +1,20 @@
 import React from 'react'
-import natsort from 'natsort'
 import Breadcrumbs from './Breadcrumbs.jsx'
 import ListItem from './ListItem.jsx'
 
 export default class DirListing extends React.Component {
   render () {
-    const items = this.props.items
-      .map(item => {
-        item.href = item.href.replace(/\/+\s*$/, '')
-        return item
-      })
-      .sort(natsort({insensitive: true}))
-
-    const directory = items
-      .filter(item => item.className === 'dir')
-
-    const file = items
-      .filter(item => item.className === 'file')
-
     return (
       <div>
         <Breadcrumbs />
-        {directory.map(item => <ListItem item={item} key={item.href}/>)}
-        {file.map(item => <ListItem item={item} key={item.href}/>)}
+        {
+          this.props.directories
+            .map(item => <ListItem item={item} key={item.href}/>)
+        }
+        {
+          this.props.files
+            .map(item => <ListItem item={item} key={item.href}/>)
+        }
       </div>
     )
   }
