617812fea1498eea1e20ff138f9f776925ea2f60
zackad <zackad.dev@gmail.com>
2019-04-21 23:17:22 +0700

    [refactor] convert DirListing into functinal component

--- src/components/DirListing.jsx
+++ src/components/DirListing.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Breadcrumbs from './Breadcrumbs.jsx'
 import ListItem from './ListItem.jsx'
 
-export default class DirListing extends React.Component {
-  render () {
-    return (
-      <div>
-        <Breadcrumbs />
-        {
-          this.props.directories
-            .map(item => <ListItem item={item} key={item.href}/>)
-        }
-        {
-          this.props.files
-            .map(item => <ListItem item={item} key={item.href}/>)
-        }
-      </div>
-    )
-  }
+function DirListing ({ directories, files }) {
+  return (
+    <div>
+      <Breadcrumbs />
+      {
+        directories
+          .map(item => <ListItem item={item} key={item.href}/>)
+      }
+      {
+        files
+          .map(item => <ListItem item={item} key={item.href}/>)
+      }
+    </div>
+  )
 }
+
+export default DirListing
