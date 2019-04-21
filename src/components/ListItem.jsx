6a3499728d0ce47a1bc47ef1993ecf286376b288
zackad <zackad.dev@gmail.com>
2019-04-21 23:13:58 +0700

    [refactor] convert into functional component

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default class ListItem extends React.Component {
-  render () {
-    return (
-      <p className='m-1'>
-        <a className='no-underline'
-          href={this.props.item.href}
-          >
-          {this.props.item.textContent}
-        </a>
-      </p>
-    )
-  }
+function ListItem ({ item }) {
+  return (
+    <p className='m-1'>
+      <a className='no-underline'
+        href={item.href}
+        >
+        {item.textContent}
+      </a>
+    </p>
+  )
 }
+
+export default ListItem
