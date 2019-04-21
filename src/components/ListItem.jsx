13a0f435ea74e1f96645c89231f6258d35b658d9
zackad <zackad.dev@gmail.com>
2019-04-21 23:41:35 +0700

    [feat] add spacing and border to list item

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ListItem ({ item }) {
   return (
-    <p className='m-1'>
+    <p className='m-1 p-2 border-b'>
       <a className='no-underline'
         href={item.href}
         >
