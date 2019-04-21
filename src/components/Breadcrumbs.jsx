b3fa5d8a5c3c0234585d02956b17da6dcfe582ce
zackad <zackad.dev@gmail.com>
2019-04-21 23:20:07 +0700

    [refactor] convert Breadcrumbs into functinal component

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 
-export default class Breadcrumbs extends React.Component {
-  render () {
-    const location = document.location.pathname
-    let items = location
-      .split('/')
-      .filter(item => item !== '')
+function Breadcrumbs () {
+  const location = document.location.pathname
+  let items = location
+    .split('/')
+    .filter(item => item !== '')
 
-    let breadcrumbs = [{path: '/', title: 'Root'}]
-    let link = ''
-    for (let item of items) {
-      link += '/' + item
-      breadcrumbs.push({
-        path: decodeURIComponent(link),
-        title: decodeURIComponent(item)
-      })
-    }
-
-    breadcrumbs.map((item, i, arr) => {
-      if (arr.length - 1 === i) {
-        item.element = <span key={item.path}> / {item.title}</span>
-      } else {
-        item.element = <span key={item.path}> / <a href={item.path}>{item.title}</a></span>
-      }
-      return item
+  let breadcrumbs = [{path: '/', title: 'Root'}]
+  let link = ''
+  for (let item of items) {
+    link += '/' + item
+    breadcrumbs.push({
+      path: decodeURIComponent(link),
+      title: decodeURIComponent(item)
     })
-
-    return (
-      <div>
-        {breadcrumbs.map(item => item.element)}
-      </div>
-    )
   }
+
+  breadcrumbs.map((item, i, arr) => {
+    if (arr.length - 1 === i) {
+      item.element = <span key={item.path}> / {item.title}</span>
+    } else {
+      item.element = <span key={item.path}> / <a href={item.path}>{item.title}</a></span>
+    }
+    return item
+  })
+
+  return (
+    <div>
+      {breadcrumbs.map(item => item.element)}
+    </div>
+  )
 }
+
+export default Breadcrumbs
