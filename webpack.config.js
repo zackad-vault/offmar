2e137159f24419c4e0094364738e831a8eca3f16
zackad <zackad.dev@gmail.com>
2019-04-21 22:50:28 +0700

    [chore] add css-loader to handle stylesheet

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
