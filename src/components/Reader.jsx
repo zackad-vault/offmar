9fa7dddb935a010264e5cb002b7cb90065049ecd
zackad <zackad.dev@gmail.com>
2019-06-13 16:23:18 +0700

    [feat] filter images to be rendered
    Add Breadcrumbs component on reader.

--- src/components/Reader.jsx
+++ src/components/Reader.jsx
@@ -1,9 +1,22 @@
 import React from 'preact'
+import Breadcrumbs from './Breadcrumbs.jsx'
+
+function Image ({ src, alt }) {
+  return (
+    <img src={src} alt={alt}
+      className='block'
+    />
+  )
+}
 
 function Reader ({ files }) {
+  const regexFilter = new RegExp('.jpe?g$|.png$|.gif$', 'i')
+  const images = files.filter(image => image.href.match(regexFilter))
+
   return (
     <div>
-      {files.map((image, index) => <img src={image} key={index} alt={image} />)}
+      <Breadcrumbs />
+      {images.map((image, index) => <Image src={image} key={index} alt={image} />)}
     </div>
   )
 }
