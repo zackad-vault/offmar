ff9a7ec4624a08d08e3f6e22ab5f7e3fb0741a43
zackad <zackad.dev@gmail.com>
2019-06-25 13:19:09 +0700

    [refactor] reduce redundant color definition on components

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -2,13 +2,13 @@ import React from 'preact'
 
 function LabelItem ({title}) {
   return (
-    <span className='text-grey'> / {title}</span>
+    <span className='text-secondary'> / {title}</span>
   )
 }
 
 function LinkItem ({path, title}) {
   return (
-    <span> / <a href={path} className='text-grey-lighter no-underline'>{title}</a></span>
+    <span> / <a href={path} className='no-underline'>{title}</a></span>
   )
 }
 
