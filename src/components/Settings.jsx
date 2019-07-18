02cfca1e2020ad3357f6a7a88997951ffec9e00b
zackad <zackad.dev@gmail.com>
2019-07-18 12:54:14 +0700

    [feat] add state to setting button

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,5 +1,20 @@
 import React from 'preact'
 
-export function SettingToggleButton() {
-  return <button>Setting Button</button>
+export class SettingToggleButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      openSetting: false,
+    }
+    this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  }
+
+  buttonClickHandler() {
+    this.setState(prevState => ({ openSetting: !prevState.openSetting }))
+  }
+
+  render() {
+    const { openSetting } = this.state
+    return <button onClick={this.buttonClickHandler}>Setting Button - {openSetting ? 'Open' : 'Close'}</button>
+  }
 }
