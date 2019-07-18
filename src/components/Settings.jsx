9ad056d4b65366ab8a5a379b074bcab86d4494f1
zackad <zackad.dev@gmail.com>
2019-07-18 13:08:53 +0700

    [feat] add SettingDialog container component

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -15,6 +15,21 @@ export class SettingToggleButton extends React.Component {
 
   render() {
     const { openSetting } = this.state
-    return <button onClick={this.buttonClickHandler}>Setting Button - {openSetting ? 'Open' : 'Close'}</button>
+    return (
+      <div className={``}>
+        <button onClick={this.buttonClickHandler}>Setting Button - {openSetting ? 'Open' : 'Close'}</button>
+        {openSetting ? <SettingDialog /> : ''}
+      </div>
+    )
+  }
+}
+
+export class SettingDialog extends React.Component {
+  render() {
+    return (
+      <div className={`fixed top-12 inset-x-0 bg-secondary p-3 border mx-auto w-1/4`}>
+        <h2 className={`font-medium`}>Settings</h2>
+      </div>
+    )
   }
 }
