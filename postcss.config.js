21aa6f621edc051db95831100a75bf5ab5e2d33d
zackad <zackad.dev@gmail.com>
2019-07-18 17:35:21 +0700

    [chore] use cssnano to optimize stylesheets

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 // postcss.config.js
 const purgecss = require('@fullhuman/postcss-purgecss')({
-
   // Specify the paths to all of the template files in your project
   content: [
     './src/**/*.html',
@@ -10,16 +9,16 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : [],
-    require('postcss-discard-comments'),
-  ]
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
 }
