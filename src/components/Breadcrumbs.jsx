ce3a2e84fc42fc8b053c841764afb793d408054e
zackad <zackad.dev@gmail.com>
2019-07-18 12:12:08 +0700

    [feat] add setting toggle button on Breadcrumbs
    Use Prettier to fix code style.

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,45 +1,50 @@
 import React from 'preact'
+import { SettingToggleButton } from './Settings.jsx'
 
-function LabelItem ({title}) {
+function LabelItem({ title }) {
+  return <span className='text-secondary'> / {title}</span>
+}
+
+function LinkItem({ path, title }) {
   return (
-    <span className='text-secondary'> / {title}</span>
+    <span>
+      {' '}
+      /{' '}
+      <a href={path} className='no-underline'>
+        {title}
+      </a>
+    </span>
   )
 }
 
-function LinkItem ({path, title}) {
-  return (
-    <span> / <a href={path} className='no-underline'>{title}</a></span>
-  )
-}
-
-function Breadcrumbs () {
+function Breadcrumbs() {
   const location = document.location.pathname
-  let items = location
-    .split('/')
-    .filter(item => item !== '')
+  let items = location.split('/').filter(item => item !== '')
 
-  let breadcrumbs = [{path: '/', title: 'Root'}]
+  let breadcrumbs = [{ path: '/', title: 'Root' }]
   let link = ''
   for (let item of items) {
     link += '/' + item
     breadcrumbs.push({
       path: decodeURIComponent(link),
-      title: decodeURIComponent(item)
+      title: decodeURIComponent(item),
     })
   }
 
   breadcrumbs.map((item, i, arr) => {
     if (arr.length - 1 === i) {
-      item.element = <LabelItem title={item.title}/>
+      item.element = <LabelItem title={item.title} />
     } else {
-      item.element = <LinkItem path={item.path} title={item.title}/>
+      item.element = <LinkItem path={item.path} title={item.title} />
     }
     return item
   })
 
   return (
-    <div className='p-2 border-b bg-secondary'>
+    <div className='p-2 border-b bg-secondary flex'>
       {breadcrumbs.map(item => item.element)}
+      <span className={`flex-grow`} />
+      <SettingToggleButton />
     </div>
   )
 }
---
+++ src/components/Settings.jsx
@@ -0,0 +1,5 @@
+import React from 'preact'
+
+export function SettingToggleButton() {
+  return <button>Setting Button</button>
+}
