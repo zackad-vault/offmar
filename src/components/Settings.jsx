14203333f6d71d742537a595d45392382732ce68
zackad <zackad.dev@gmail.com>
2019-07-18 14:47:41 +0700

    [refactor] change SettingDialog into stateless component

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -10,15 +10,13 @@ export function SettingToggleButton() {
   )
 }
 
-export class SettingDialog extends React.Component {
-  render() {
-    return (
-      <div className={`fixed top-12 inset-x-0 bg-secondary p-3 border mx-auto w-1/4`}>
-        <h2 className={`font-medium`}>Settings</h2>
-        <hr className={`border-b`} />
-        <button className={`button border py-1 px-2 ml-2 text-xs float-right`}>Save Settings</button>
-        <button className={`button border py-1 px-2 text-xs float-right`}>Discard Changes</button>
-      </div>
-    )
-  }
+export function SettingDialog() {
+  return (
+    <div className={`fixed top-12 inset-x-0 bg-secondary p-3 border mx-auto w-1/4`}>
+      <h2 className={`font-medium`}>Settings</h2>
+      <hr className={`border-b`} />
+      <button className={`button border py-1 px-2 ml-2 text-xs float-right`}>Save Settings</button>
+      <button className={`button border py-1 px-2 text-xs float-right`}>Discard Changes</button>
+    </div>
+  )
 }
