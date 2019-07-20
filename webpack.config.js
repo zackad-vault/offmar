bb238010ec7d28f574876980dc83e94a8cd74809
zackad <zackad.dev@gmail.com>
2019-07-20 07:53:18 +0700

    [chore] remove .babelrc file and place it's config inside webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/preset-react'],
+          },
         },
       },
     ],
